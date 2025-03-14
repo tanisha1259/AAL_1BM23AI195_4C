51cd08a7eec59d4b2c356e1669f0407ec1668c44
tanisha1259 <tanishaprakash259@gmail.com>
2025-03-14 23:52:56 +0530

    Binarysearch.c

--- Binarysearch.c
+++ Binarysearch.c
@@ -2,33 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n) 
+{
     int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for (i=0; i<n-1; i++) 
+    {
+        for (j=0; j<n-i-1; j++) 
+        {
+            if (arr[j]>arr[j+1]) 
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
     }
 }
-int binarySearch(int arr[], int n, int key) {
-    int left = 0, right = n - 1, mid;
-    while (left <= right) {
-        mid = left + (right - left) / 2;
-        if (arr[mid] == key) return mid;
-        if (arr[mid] < key) left = mid + 1;
-        else right = mid - 1;
+int binarySearch(int arr[], int n, int key) 
+{
+    int left=0, right=n-1, mid;
+    while (left<=right) 
+    {
+        mid=left+(right-left)/2;
+        if (arr[mid]==key) return mid;
+        if (arr[mid]<key) left=mid+1;
+        else right=mid-1;
     }
     return -1;
 }
-int main() {
-    int n = 20, arr[20], key, i;
+int main() 
+{
+    int n=20, arr[20], key, i;
     srand(time(0));
 
-    for (i = 0; i < n; i++) arr[i] = rand() % 100 + 1;
+    for (i=0; i<n; i++) arr[i]=rand()%100+1;
 
     printf("Generated random numbers: ");
     for (i = 0; i < n; i++) printf("%d ", arr[i]);
